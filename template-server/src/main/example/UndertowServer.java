27c58da6f70f26dc88adc67288fbf007cb864967
Ernest <ezeidman@palantir.com>
2019-01-01 19:16:05 -0500

    starting to implement the server

--- template-server/src/main/example/UndertowServer.java
+++ template-server/src/main/example/UndertowServer.java
@@ -5,6 +5,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UndertowServer {
+    public static void main(String[] args) {
+
+        PathHandler path = Handlers.path();
+
+        server = Undertow.builder()
+                .addHttpListener(port, "localhost")
+                .setHandler(path)
+                .build();
 
 
+    }
 }
